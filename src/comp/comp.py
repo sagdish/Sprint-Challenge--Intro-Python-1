7592e1586c4b3424f83c91b10c3baa4eff633af0
sagdish <forsi1@yahoo.com>
2020-05-15 19:27:39 -0700

    comprehension ready

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -25,19 +25,19 @@ print(humans[0].name)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [human for human in humans if human.name.startswith('D') ]
+a = [human.name for human in humans if human.name.startswith('D') ]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = [human for human in humans if human.name.endswith('e') ]
+b = [human.name for human in humans if human.name.endswith('e') ]
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [human for human in humans if human.name[0] >= "C" and human.name[0] <= "G"]
+c = [human.name for human in humans if human.name[0] >= "C" and human.name[0] <= "G"]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -68,5 +68,6 @@ print(g)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = [math.floor(math.sqrt(human.age)) for human in humans]
+h = [math.sqrt(human.age) for human in humans]
+# h = [math.floor(math.sqrt(human.age)) for human in humans] # nicer way 
 print(h)
